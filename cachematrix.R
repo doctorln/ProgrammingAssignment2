55c1997a4229d18dfaddb921831eb1d9ccd7d59c
doctorln <doctorln@yahoo.com>
2016-05-09 03:53:29 +0100

    makeCacheMatrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      im <- NULL ##set initial value of the matrix inverse as NULL
+      set <- function(y) {
+            x <<- y
+            im <<- NULL
+      }
+      get <- function() x ##get the value of the matrix
+      setim <- function(solve) im <<- solve ##set the value of the inverse
+      getim <- function() im ##get the value of the inverse
+      list(set = set, get = get, setim = setim, getim = getim)
 }
 
 
@@ -12,4 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+      im <- x$getim()
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache
+      if (!is.null(im)){
+            return(im)
+      }
+      else {
+            data <- x$get()
+            im<- solve(data, ...)
+            x$setim(im)
+            
+      }
+      im 
 }
